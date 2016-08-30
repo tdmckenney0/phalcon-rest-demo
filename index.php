de1d05bae90acefa374bc170c0efd47035ddf4b5
Tanner Mckenney <tmckenney@outlook.com>
2016-08-30 15:19:53 -0400

    Fixed Reads
    Fixed read APIs. Still need to correct POST and PUT functions.

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $di->set('db', function() {
     );
 });
 
-$app = new Micro();
+$app = new Micro($di);
 
 $app->get('/api/robots', function() use ($app) { 
 
@@ -49,7 +49,7 @@ $app->get('/api/robots', function() use ($app) {
 
 $app->get('/api/robots/search/{name}', function($name) use ($app) {
 
-    $phql = "SELECT * FROM Robots WHERE name LIKE :name ORDER BY name";
+    $phql = "SELECT * FROM Robots WHERE name LIKE :name: ORDER BY name";
     $robots = $app->modelsManager->executeQuery($phql, array('name' => '%' . $name . '%'));
 
     $data = array();
@@ -67,7 +67,7 @@ $app->get('/api/robots/search/{name}', function($name) use ($app) {
 
 $app->get('/api/robots/{id:[0-9]+}', function ($id) use ($app) {
 
-    $phql = "SELECT * FROM Robots WHERE id = :id";
+    $phql = "SELECT * FROM Robots WHERE id = :id:";
     $robot = $app->modelsManager->executeQuery($phql, array('id' => $id))->getFirst();
 
     $response = new Response();
@@ -88,16 +88,120 @@ $app->get('/api/robots/{id:[0-9]+}', function ($id) use ($app) {
 
 });
 
-$app->post('/api/robots', function () {
+$app->post('/api/robots', function () use ($app) {
+
+    $robot = $app->request->getJsonRawBody();
+
+    $phql = "INSERT INTO Robots (name, type, year) VALUES (:name:, :type:, :year:)";
+
+    $status = $app->modelsManager->executeQuery($phql, array(
+        'name' => $robot->name,
+        'type' => $robot->type,
+        'year' => $robot->year
+    ));
+    
+    $response = new Response();
+
+    if($status->success() == true) {
+
+        $response->setStatusCode(201, "created");
+
+        $robot->id = $status->getModel()->id;
+
+        $response->setJsonContent(array(
+            'status' => 'OK',
+            'data' => $robot
+        ));
+
+    } else {
+
+        $response->setStatusCode(409, "Conflict");
+
+        $errors = array();
+
+        foreach($status->getMessages() as $message) {
+            $errors[] = $message->getMessage();
+        }
+
+        $response->setJsonContent(array(
+            'status' => 'ERROR',
+            'messages' => $errors
+        ));
+
+    }
+
+    return $response;
 
 });
 
-$app->put('/api/robots/{id:[0-9]+}', function () {
+$app->put('/api/robots/{id:[0-9]+}', function ($id) use ($app) {
+
+    $robot = $app->request->getJsonRawBody();
+
+    $phql = "UPDATE Robots SET name = :name:, type = :type:, year = :year: WHERE id = :id:";
+    $status = $app->modelsManager->executeQUery($phql, array(
+        'id' => $id,
+        'name' => $robot->name,
+        'type' => $robot->type,
+        'year' => $robot->year
+    ));
+
+    $response = new Response();
+
+    if($status->success() == true) {
+
+        $response->setJsonContent(array(
+            'status' => 'OK'
+        ));
+
+    } else {
+
+        $response->setStatusCode(409, "Conflict");
+
+        $errors = array();
+
+        foreach($status->getMessages() as $message) {
+            $errors[] = $message->getMessage();
+        }
+
+        $response->setJsonContent(array(
+            'status' => 'ERROR',
+            'messages' => $errors
+        ));
+    }
+
+    return $response;
 
 });
 
-$app->delete('/api/robots/{id:[0-9]+}', function () {
+$app->delete('/api/robots/{id:[0-9]+}', function ($id) use ($app) {
 
+    $phql = "DELETE FROM Robots WHERE id = :id:";
+    $status = $app->modelsManager->executeQuery($phql, array('id' => $id));
+
+    $response = new Response();
+
+    if($status->success() == true) {
+
+        $response->setJsonContent(array('status' => 'OK'));
+
+    } else {
+
+        $response->setStatusCode(409, "Conflict");
+
+        $errors = array();
+
+        foreach($status->getMessages() as $message) {
+            $errors[] = $message->getMessage();
+        }
+
+        $response->setJsonContent(array(
+            'status' => 'ERROR',
+            'messages' => $errors
+        ));
+    }
+
+    return $response;
 });
 
 $app->handle();
